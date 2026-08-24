e32e2685557a735d5bdb8736d057a27ec69f1b0a
Mantosh <mahiraval000@gmail.com>
2026-08-25 01:45:38 +0530

    Add RabbitMQ consumer to process jobs, switch to JacksonJsonMessageConverter

--- src/main/java/com/relay/relay/service/JobConsumerService.java
+++ src/main/java/com/relay/relay/service/JobConsumerService.java
@@ -43,7 +43,6 @@ public class JobConsumerService {
     }
 
     private void processJob(JobMessageDTO message) throws InterruptedException {
-        // Abhi ke liye simulate kar rahe hain actual kaam
         System.out.println("Processing job type: " + message.getJobType());
         System.out.println("Payload: " + message.getPayload());
         Thread.sleep(2000); // 2 second ka fake processing time
